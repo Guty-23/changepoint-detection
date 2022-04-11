78ecff933b1c77e6f601b3a7d0695c7853dbad65
Facundo <facumgutierrez@gmail.com>
2022-04-11 03:10:24 -0300

    some constants

--- implementation/src/utils/constants.py
+++ implementation/src/utils/constants.py
@@ -8,13 +8,14 @@ class Constants:
     This represents an input where
     existent changepoints are to be located
     """
-    seed = 23
-    cases_per_type = 10
-    mean_limit = 15
-    batch_size = 1000
-    std_limit = 8
-    min_days = 2
-    minutes_in_a_day = 24 * 60
-    project_root_path = os.path.dirname(os.path.abspath(__file__)) + '/../../'
-    random_path = project_root_path + 'resources/cases/random/'
-    real_path = project_root_path + 'resources/cases/real/'
+    seed: int = 23
+    cases_per_type: int = 10
+    mean_limit: int = 15
+    batch_size: int = 1000
+    std_limit: int = 8
+    min_days: int = 2
+    minutes_in_a_day: int = 24 * 60
+    kernel_bandwith: float = 1e-3
+    project_root_path: str = os.path.dirname(os.path.abspath(__file__)) + '/../../'
+    random_path: str = project_root_path + 'resources/cases/random/'
+    real_path: str = project_root_path + 'resources/cases/real/'
