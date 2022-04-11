9db316a4fe85a1f2fb6b3c85a5e529f993f4fb65
Facundo <facumgutierrez@gmail.com>
2022-04-11 02:24:50 -0300

    adjust gaussian range cost

--- implementation/src/cost_functions/cost_function.py
+++ implementation/src/cost_functions/cost_function.py
@@ -37,8 +37,8 @@ class GaussianCostFunction(CostFunction):
         self.prefix_sum = accumulate(signal)
         self.prefix_sum_squares = accumulate(signal, lambda x: x ** 2)
 
-    def range_cost(self, i: int, j: int) -> float:
-        inv_length = 1.0 / float(j-i)
-        linear_term = inv_length * (self.prefix_sum[j] - self.prefix_sum[i])
-        square_term = (inv_length ** 2) * (self.prefix_sum_squares[j] - self.prefix_sum_squares[i])
-        return linear_term + square_term
+    def range_cost(self, start: int, end: int) -> float:
+        inv_length = 1.0 / float(end-start)
+        linear_sum_term = (inv_length ** 2) * ((self.prefix_sum[end] - self.prefix_sum[start]) ** 2)
+        square_sum_term = inv_length * (self.prefix_sum_squares[end] - self.prefix_sum_squares[start])
+        return square_sum_term - linear_sum_term
